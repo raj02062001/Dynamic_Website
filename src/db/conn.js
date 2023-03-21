fa12c8c32c20d0e4ae0e6dd95e9337a8aa5b2425
Raj <rajvakhariya23@gmail.com>
2023-03-21 12:04:01 +0530

    Init Commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/RajDynamic")
+  .connect(
+    "mongodb+srv://raj:raj@atlascluster.buxi0b2.mongodb.net/RajDynamic?",
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
+  )
   .then(() => {
     console.log("Connection Successful");
   })
